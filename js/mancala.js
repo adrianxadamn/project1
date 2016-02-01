56a0732d6b448a93413500473fe6eca91b967064
Adrian Nuyda <adamnuyda@yahoo.com>
2016-01-31 20:16:30 -0800

    can now rotate back to index0 if index is 13

--- js/mancala.js
+++ js/mancala.js
@@ -15,18 +15,32 @@ var move = function(seedIndex) {
     console.log("seeds in hand =" + " " + seedsInHand);
     //phase 2 player adds 1 seed to each incoming pit from seedsInHand
  for (var i = seedsInHand; i > 0; i--) {
+    if (seedIndex === 13) {
+      seedIndex = 0;
+    } else {
     seedIndex += 1;
+    }
     if (seedIndex === 6 && turn === "player1") {
       board[seedIndex] += 1;
-      console.log("added 1 seed to player1's store")
-      console.log("seeds in hand =" + " " + (i - 1) )
+      console.log("added 1 seed to player1's store");
+      console.log("seeds in hand =" + " " + (i - 1));
       console.log(board);
-    } else {
+    } else if (seedIndex === 6 && turn === "player2") {
+      seedIndex = 7;
+      board[seedIndex] += 1;
+      console.log("added 1 seed to pit");
+      console.log("seeds in hand =" + " " + (i - 1));
+      console.log(board);
+    }
+
+
+      else {
     board[seedIndex] ++;
     console.log("added 1 seed to pit")
     console.log("seeds in hand =" + " " + (i - 1) )
     console.log(board)
   }
+
 
 
 
